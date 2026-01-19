040348e1044909fc3e4c7ab9fb0606b2162020ac
LoS <lostreit@pm.me>
2026-01-19 15:02:18 +0100

    first commit

---
+++ homework.js
@@ -0,0 +1,22 @@
+// #1
+// const firstName = "Lo"
+// const favNum = 32
+// const likeCoding = true
+// console.log(`Hi, my name is ${firstName}, my favorite number is ${favNum}, and it is ${likeCoding} that I like coding.`)
+
+// #2
+// const birthYear = 1990
+// const currentYear = 2026
+// const myAge = currentYear - birthYear
+// console.log(`I am ${myAge} years old!`)
+
+// #3
+const myFood = ["Pasta", "Corn", "Rice"]
+const student = {
+    firstName: "Lo",
+    favNum: 32,
+    likeCoding: true,
+    myFood
+}
+// console.log(student) // Print whole object
+console.log(student.firstName) // Print firstName property
---
+++ index.js
@@ -0,0 +1,24 @@
+// const firstName = "Lo"
+// const age = 36
+// const isEnby = true
+// const pronouns = "they/them"
+// console.log(firstName)
+// console.log(age)
+// console.log(isEnby)
+// console.log(`my name is ${firstName}. I am ${age} years old.`)
+// console.log(`I am non-binary and use ${pronouns} pronouns.`)
+
+// const favFood = "Pasta"
+// const weekMeals = 4
+// const cook = true;
+
+// console.log("My favorite food is " + favFood + ", I eat it " + weekMeals + " times a week." + " It is " + cook + " that I can cook it myself!")
+
+// const points = 19;
+// console.log("CoursePoints: " + (points + 10))
+
+const baseUrl = "https://svt.se/"
+const userId = "12345"
+const userUrl = baseUrl + "user/" + userId
+
+console.log("User URL: " + userUrl)
